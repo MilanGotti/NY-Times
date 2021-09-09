6af242344338ae636bb81d58f61d78b037bed7dc
Milan goti <87664566+MilanGotti@users.noreply.github.com>
2021-09-09 21:13:31 +0530

    adding three faces

---
+++ NYArticles/ProjectManager/Controllers/TabBarController/AppTabBarViewController.swift
@@ -0,0 +1,21 @@
+//
+//  AppTabBarViewController.swift
+//  NYArticles
+//
+//  Created by DREAMWORLD on 09/09/21.
+//
+
+import UIKit
+
+class AppTabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.tabBar.barTintColor = .appBlue
+        self.tabBar.isTranslucent = false
+        
+    }
+    
+
+}
